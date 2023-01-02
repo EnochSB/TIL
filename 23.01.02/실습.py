2271791430bc4d367087871aaf6f3a253c1f5be3
unknown <gktjd425@naver.com>
2023-01-02 17:53:41 +0900

    파이썬기초

---
+++ 23.01.02/실습.py
@@ -0,0 +1,65 @@
+#1번
+num1 = int(input('정수형 숫자를 입력해주세요 > '))
+print(num1 + 10)
+
+#2번
+food = input('좋아하는 음식을 입력해주세요 > ')
+print('좋아하는 음식 : ', food)
+
+#3번
+name = input('이름을 입력해주세요 > ')
+year = int(input('태어난 년도를 입력해주세요 > '))
+print('저의 이름은 ', name, '이고, 올해 나이는 ', 2024-year, '세 입니다.', sep= '')
+
+#4번
+str1 = input('첫 번째 문장을 입력해주세요 > ')
+str2 = input('두 번째 문장을 입력해주세요 > ')
+print(str1 + str2)
+
+#5번
+num2 = int(input('정수형 숫자를 입력해주세요 > '))
+print(-num2)
+
+#6번
+num3 = int(input('첫 번째 정수형 숫자를 입력해주세요 > '))
+num4 = int(input('두 번째 정수형 숫자를 입력해주세요 > '))
+print('더하기연산 :', num3 + num4)
+print('빼기 연산 : ', num3 - num4)
+print('곱하기 연산 : ', num3 * num4)
+print('몫 연산 : ', num3 // num4)
+print('나머지 연산 : ', num3 % num4)
+
+#7번
+num5 = int(input('첫 번째 정수형 숫자를 입력해주세요 > '))
+num6 = int(input('두 번째 정수형 숫자를 입력해주세요 > '))
+num7 = int(input('세 번째 정수형 숫자를 입력해주세요 > '))
+num8 = num5 + num6 + num7
+print(num8/3)
+
+#8번
+num9 = int(input('첫 번째 정수형 숫자를 입력해주세요 > '))
+num10 = int(input('두 번째 정수형 숫자를 입력해주세요 > '))
+num11 = int(input('세 번째 정수형 숫자를 입력해주세요 > '))
+num12 = int(input('네 번째 정수형 숫자를 입력해주세요 > '))
+num13 = int(input('다섯 번째 정수형 숫자를 입력해주세요 > '))
+
+list1 = [num9, num10, num11, num12, num13]
+print(list1)
+
+#9번
+str3 = input('문자열을 입력해주세요 > ')
+num14 = int(input('정수형 숫자를 입력해주세요 > '))
+print(str3 * num14)
+
+#10번
+num = 0
+num += int(input('첫 번째 정수형 숫자를 입력해주세요 > '))
+print(num)
+num += int(input('두 번째 정수형 숫자를 입력해주세요 > '))
+print(num)
+num += int(input('세 번째 정수형 숫자를 입력해주세요 > '))
+print(num)
+num += int(input('네 번째 정수형 숫자를 입력해주세요 > '))
+print(num)
+num += int(input('다섯 번째 정수형 숫자를 입력해주세요 > '))
+print(num)

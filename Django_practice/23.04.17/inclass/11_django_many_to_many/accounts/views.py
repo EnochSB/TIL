84362f6044dd230c155ab9210875cace416b95f9
SungBum Ha <gktjd425@naver.com>
2023-04-22 23:34:52 +0900

    django_mtm_practice

--- Django_practice/23.04.17/inclass/11_django_many_to_many/accounts/views.py
+++ Django_practice/23.04.17/inclass/11_django_many_to_many/accounts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
-from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth import update_session_auth_hash, get_user_model
 from django.contrib.auth.decorators import login_required
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 
@@ -82,3 +82,26 @@ def change_password(request):
         'form': form,
     }
     return render(request, 'accounts/change_password.html', context)
+
+
+@login_required
+def profile(request, username):
+    User = get_user_model()
+    person = User.objects.get(username=username)
+    context = {
+        'person': person,
+    }
+    return render(request, 'accounts/profile.html', context)
+
+
+@login_required
+def follow(request, user_pk):
+    User = get_user_model()
+    you = User.objects.get(pk=user_pk)
+    me = request.user
+    if you != me:
+        if you.followers.filter(pk=request.user.pk).exists():
+            you.followers.remove(request.user)
+        else:
+            you.followers.add(request.user)
+    return redirect('accounts:profile', you.username)

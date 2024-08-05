d18e1135f352c8c7a5ad93d3d3c21e98b3d5a7f7
Enoch <gktjd425@naver.com>
2024-08-05 20:32:18 +0900

    TIL_24.08.05_django_crud

--- 24.08.05/MySite/urls.py
+++ 24.08.05/MySite/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for Restart project.
+URL configuration for MySite project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
